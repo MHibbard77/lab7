58124db14e5195c7070b160494201f2c7b84c357
Jeffrey Wolf <lwolf@uwm.edu>
2019-03-06 14:10:02 -0600

    instructor

---
+++ instructor.py
@@ -0,0 +1,11 @@
+from Person import Person
+
+
+class Instructor(Person):
+
+    def __init__(self, email, username, password):
+        super().__init__(email, username, password)
+        self.name = "DEFAULT"
+        self.phone = "DEFAULT"
+        self.courses = []
+

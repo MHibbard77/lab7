8e58c89938b22067386e7cb5d3c95dea45326848
EvanVetrano <evanv1998@gmail.com>
2019-03-06 14:15:24 -0600

    committing!

---
+++ test_supervisor.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestSupervisor(TestCase):
+    def test_assign_instructor_course(self):
+        self.fail()
+
+    def test_assign_ta_course(self):
+        self.fail()
+
+    def test_assign_ta_lab(self):
+        self.fail()

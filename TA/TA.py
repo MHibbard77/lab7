749a49e9c5357e633a9a99fb959b1e030a235950
smit2633 <smit2633@uwm.edu>
2019-03-06 14:07:35 -0600

    TA class

---
+++ TA/TA.py
@@ -0,0 +1,14 @@
+from Person import Person
+
+
+class TA(Person):
+
+    def __init__(self, email, username, password):
+        super().__init__(email, username, password)
+        self.name = "Default"
+        self.phone = "Default"
+        self.courses = []
+
+    def __view_TA_assignments__(self, assignments):
+
+

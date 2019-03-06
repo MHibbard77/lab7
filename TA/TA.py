a1f37e65e2b49b3ca01da4986053ab78d7eb7b20
smit2633 <smit2633@uwm.edu>
2019-03-06 14:37:08 -0600

    TA class

--- TA/TA.py
+++ TA/TA.py
@@ -5,8 +5,8 @@ class TA(Person):
 
     def __init__(self, email, username, password):
         super().__init__(email, username, password)
-        self.name = "Default"
-        self.phone = "Default"
+        self.name = "DEFAULT"
+        self.phone = "DEFAULT"
         self.courses = []
 
     def __view_TA_assignments__(self, assignments):
--- TA/test_TA.py
+++ TA/test_TA.py
@@ -3,8 +3,13 @@ from TA import TA
 
 
 class TestTA(TestCase):
-    def test___init__(self):
-        self.fail()
+    def test__init__(self):
+        self.ta1 = TA("DEFAULT_EMAIL", "DEFAULT_USERNAME", "DEFAULT_PASSWORD")
+        self.assertEquals(self.ta1.email, "DEFAULT_EMAIL")
+        self.assertEquals(self.ta1.username, "DEFAULT_USERNAME")
+        self.assertEquals(self.ta1.password, "DEFAULT_PASSWORD")
+        self.assertEquals(self.ta1.name, "DEFAULT")
+        self.assertEquals(self.ta1.phone_number, "DEFAULT")
 
     def test___view_TA_assignments__(self):
         self.fail()
